81aaee64c003cfb7ed607370de9912771595cfbe
LasithaRanathunga <86787587+LasithaRanathunga@users.noreply.github.com>
2025-07-03 22:31:18 +0530

    client initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import Home from "./pages/Home";
+
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;

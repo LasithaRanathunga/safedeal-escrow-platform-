6da97e876ab7b99a5f22a9d1f3324f4cd7655c26
LasithaRanathunga <86787587+LasithaRanathunga@users.noreply.github.com>
2025-07-04 22:57:37 +0530

    shadcn added

---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import logoImg from "../assets/d2cffbb4-5fb2-4ff3-84d1-7034c6d0aac2_removalai_preview.png";
+
+export default function Logo() {
+  return (
+    <div>
+      <img src={logoImg} alt="Logo" />
+    </div>
+  );
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+import Logo from "@/components/Logo";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Home</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Home</h1>
+      <Button>Hello</Button>
+      <Logo />
+    </>
+  );
 }

d89f55351566493e7171b11701c33af07dd17eb2
LasithaRanathunga <86787587+LasithaRanathunga@users.noreply.github.com>
2025-07-05 14:41:51 +0530

    client/header added

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -2,9 +2,22 @@ import React from "react";
 
 import logoImg from "../assets/d2cffbb4-5fb2-4ff3-84d1-7034c6d0aac2_removalai_preview.png";
 
-export default function Logo() {
+type logoProps = { size?: "sm" | "md" | "lg" };
+
+const getSize = function (size: string) {
+  switch (size) {
+    case "sm":
+      return "w-56";
+    case "md":
+      return "w-80";
+    case "lg":
+      return "w-96";
+  }
+};
+
+export default function Logo({ size = "sm" }: logoProps) {
   return (
-    <div>
+    <div className={`${getSize(size)}`}>
       <img src={logoImg} alt="Logo" />
     </div>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,9 @@
-import React from "react";
-
-import Logo from "@/components/Logo";
-
-import { Button } from "@/components/ui/button";
+import Header from "@/containers/Header";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Home</h1>
-      <Button>Hello</Button>
-      <Logo />
+      <Header />
     </>
   );
 }

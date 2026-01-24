a07e7d2e17f9ed69b1a2a6cf59033b6c80e8f126
LasithaRanathunga <86787587+LasithaRanathunga@users.noreply.github.com>
2026-01-24 23:36:06 +0530

    [test:unit] wrote unit test for authService.createUser function

---
+++ server/test/repositories/userRepository.test.ts
@@ -0,0 +1,51 @@
+import { describe, it, expect, afterAll, beforeAll } from "vitest";
+import { createUser, getUserByEmail } from "../../repositories/userRepository";
+import db from "../../db/db";
+
+describe("userRepository", () => {
+  const testUser = {
+    email: "test@example.com",
+    name: "Test User",
+    password: "12345678",
+  };
+
+  beforeAll(async () => {
+    await db.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await db.user.deleteMany();
+  });
+
+  describe("createUser", () => {
+    it("creates a new user successfully", async () => {
+      const user = await createUser(
+        testUser.email,
+        testUser.name,
+        testUser.password,
+      );
+
+      expect(user).toMatchObject(testUser);
+    });
+
+    it("throws an error when creating a user with an existing email", async () => {
+      const testUser2 = {
+        email: testUser.email,
+        name: "Test User 2",
+        password: "12345678",
+      };
+
+      await expect(
+        createUser(testUser2.email, testUser2.name, testUser2.password),
+      ).rejects.toThrow("Unique constraint failed");
+    });
+  });
+
+  describe("getUserByEmail", () => {
+    it("retrieves the user by email successfully", async () => {
+      const user = await getUserByEmail(testUser.email);
+
+      expect(user).toMatchObject(testUser);
+    });
+  });
+});

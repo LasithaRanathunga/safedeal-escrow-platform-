8b64a7346a5eee606955fe5fbb94071a7bbddb40
LasithaRanathunga <86787587+LasithaRanathunga@users.noreply.github.com>
2026-03-03 19:39:08 +0530

    created the pull-request-ci workflow

---
+++ server/eslint.config.mjs
@@ -0,0 +1,13 @@
+import config from "eslint-config-xo";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  config,
+  {
+    files: ["**/*.json", "**/*.jsonc"],
+    language: "json/jsonc",
+    languageOptions: {
+      allowTrailingCommas: true,
+    },
+  },
+]);
